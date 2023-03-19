3532c8f8fdb0f7a5a9f2f1c22be85f3f23086ace
Behruz_Qalqonov <102896286+behruz12005@users.noreply.github.com>
2023-03-19 12:46:38 +0500

    load

---
+++ index.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import tensorflow as tf
+from PIL import Image as PILImage
+import cv2
+import numpy as np
+from keras.models import load_model
+
+
+# Tasvir qabul qilish oynasi
+image_file = st.file_uploader("Tasvirni yuklang", type=["jpg", "jpeg", "png"])
+if image_file is not None:
+    img = PILImage.open(image_file)
+
+
+# Tasvirni ko'rish uchun funksiya
+def view_image(image):
+    st.image(image, width=50,caption='Tasvir', use_column_width=True)
+
+# Modelni boshqarish uchun funksiya
+def load_model():
+    model = tf.keras.models.load_model('my_model.h5')
+    return model
+
+
+if image_file is not None:
+    image_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
+    # Tasvirni OpenCV kutubxonasi yordamida yuklash
+    tasvir = cv2.imdecode(image_bytes, cv2.IMREAD_COLOR)
+# Tasvir qabul qilishni tekshirish
+
+
+
+if image_file is not None:
+    CATEGORIES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z']
+    # Tasvirni yuklash va ko'rsatish
+
+
+    IMG_SIZE = 70
+    image = Image.open(image_file)
+    view_image(image)
+    img = np.array(img)
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+    img = img.reshape(-1, 70, 70, 1)
+    img = img / 255.0
+    model = load_model()
+
+
+    # Tasvirlarni sinash va natijani ko'rsatish
+    predictions = model.predict(img)
+    natija=CATEGORIES[np.argmax(predictions)]
+    # Tasvirni konsolga chiqarish
+    st.write(f"<p style='text-align: center; font-size: 80px;'>{natija}</p>", unsafe_allow_html=True)
+    # st.write(f'Natija: {natija}')
+else:
+    st.write("Iltimos, tasvirni yuklang!")
+
